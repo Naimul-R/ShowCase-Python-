b42b9467ec9bd41f0e21eccf01c60097edbc49e1
Naimul-R <naimul.rabby08@gmail.com>
2026-01-05 17:24:26 +0600

    Working....

--- SLOTMACHINE/main.py
+++ SLOTMACHINE/main.py
@@ -1,1 +1,12 @@
-print("My name is khan")
+def deposite():
+    while True:
+        Amount = input("What would you like to deposite? $")
+        if Amount.isdigit():
+            Amount = int(Amount)
+            if Amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0")
+        else:
+            print("Please enter a number.")
+    return Amount 
