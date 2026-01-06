9b773bc634786b80e0088a4215d5b0b55a897f94
Naimul-R <naimul.rabby08@gmail.com>
2026-01-06 23:26:51 +0600

    work on this project

--- SLOTMACHINE/main.py
+++ SLOTMACHINE/main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 def deposite():
     while True:
         Amount = input("What would you like to deposite? $")
@@ -10,3 +12,23 @@ def deposite():
         else:
             print("Please enter a number.")
     return Amount 
+
+def get_number_of_lines():
+    while True:
+        Lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if Lines.isdigit():
+            Lines = int(Lines)
+            if 1 <= Lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+    return Lines
+
+
+def main():
+    balance = deposite()
+    lines = get_number_of_lines()
+    print(balance, lines)
+main()
