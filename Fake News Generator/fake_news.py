24f9db2a21c39ff1b755bf1f154887c9951a9a08
Naimul-R <naimul.rabby08@gmail.com>
2026-01-09 23:25:53 +0600

    create all subject and using list

---
+++ Fake News Generator/fake_news.py
@@ -0,0 +1,32 @@
+import random
+
+#Create Subject
+subject = [
+    "Ronaldo",
+    "Messi",
+    "Neymar",
+    "Real Madrid",
+    "Barcelona",
+    "La Liga",
+    "Premier League",
+]
+
+action =[
+    "wins the championship",
+    "signs a new contract",
+    "scores a hat-trick",
+    "gets injured during training",
+    "announces retirement",
+    "not playing next season",
+    "going to win major trophy"
+]
+
+places_or_things =[
+    "at the final match",
+    "in the transfer market",
+    "during the press conference",
+    "in the upcoming season",
+    "at the training ground",
+    "in the World Cup",
+    "in the Champions League"
+]
