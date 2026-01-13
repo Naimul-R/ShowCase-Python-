8a781c5a57f9dee92c7005bbd214a53b39c9053f
Naimul-R <naimul.rabby08@gmail.com>
2026-01-13 21:15:43 +0600

    For now it's done with this mini project

--- Coffee-Canvas/Coffee-Menu.py
+++ Coffee-Canvas/Coffee-Menu.py
@@ -19,9 +19,23 @@ print("Espresso: BDT160\nCappuccino: BDT250\nLatte: BDT260\nAmericano: BDT155\nM
 
 order_total = 0
 
+#Ask for the item using condition
 item_1 = input("Enter the name of item you want to order: ")
 if item_1 in Menu:
     order_total += Menu[item_1]
     print(f"Your item {item_1} has been added to your order ")
 else:
     print(f"Your Ordered {item_1} is not available yet")
+
+#Ask another item using another condition 
+another_order = input("Do you want to add another item? (yes/no) ")
+if another_order == "yes":
+    item_2 = input("Enter the name of secound item = ")
+    if item_2 in Menu:
+        order_total += Menu[item_2]
+        print(f"Your item {item_2} has been added to the order.")
+    else:
+        print(f"Ordered item {item_2} is not available!")
+
+
+print(f"The total amount of items to pay is {order_total}")
