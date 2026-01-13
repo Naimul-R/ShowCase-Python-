56e691337d162a781f376716390f9108c6d046c2
Naimul-R <naimul.rabby08@gmail.com>
2026-01-13 20:40:05 +0600

    Create Menu list in the Menu

---
+++ Coffee-Canvas/Coffee-Menu.py
@@ -0,0 +1,13 @@
+#Define the menu of Coffee
+Menu = {
+    "Espresso": 160,
+    "Cappuccino": 250,
+    "Latte": 280,
+    "Americano": 155,
+    "Mocha": 300,
+    "Macchiato": 220,
+    "Flat white": 280,
+    "Cold Brew": 300,
+    "Affogato": 350,
+    "Caramel Latte": 280,
+}
