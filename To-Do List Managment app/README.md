a7b81a4a4eaa4069efb7b522a6467c2b3407c046
Naimul-R <naimul.rabby08@gmail.com>
2026-01-15 15:20:16 +0600

    define what are the funciton that I want to create and also have plan in the README.md file

---
+++ To-Do List Managment app/README.md
@@ -0,0 +1,6 @@
+## Load existing item
+
+- Create a new item
+- list item
+- mark item as complete
+- save item
---
+++ To-Do List Managment app/main.py
@@ -0,0 +1,14 @@
+def load_tasks():
+    pass
+
+def save_tasks():
+    pass
+
+def view_tasks():
+    pass
+
+def create_task():
+    pass
+
+def mark_task_complete():
+    pass
