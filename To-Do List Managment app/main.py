0f2826450594ece4dd9a591e1e9a9e992151455e
Naimul-R <naimul.rabby08@gmail.com>
2026-01-16 15:00:06 +0600

    complete main function

--- To-Do List Managment app/main.py
+++ To-Do List Managment app/main.py
@@ -12,3 +12,29 @@ def create_task():
 
 def mark_task_complete():
     pass
+
+def main():
+    tasks = load_tasks
+
+    while True:
+        print("\nTo-Do List Manager")
+        print("1. View Tasks")
+        print("2. Add Task")
+        print("3. Complete Task")
+        print("4. Exit")
+
+        choice = input("Enter your choice: ").strip()
+
+        if choice == "1":
+            view_tasks()
+        elif choice == "2":
+            create_task()
+        elif choice == "3":
+            mark_task_complete()
+        elif choice == "4":
+            print("Goodbye!")
+            break 
+        else:
+            print("Invalid Choice! Please try again.")
+
+main()
