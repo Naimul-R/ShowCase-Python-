0a75799c906c0413d4e9863db6a9af2d2096f2e1
Naimul-R <naimul.rabby08@gmail.com>
2026-01-25 12:12:51 +0600

    guess number project is now ready to play

--- Guess-Number/main.py
+++ Guess-Number/main.py
@@ -2,7 +2,7 @@ import random
 
 #Here we guess the number 
 def guess(x):
-    random_number = random.randint(1. x)
+    random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
         guess = int(input(f"Guess a number between 1 and {x}: "))
@@ -16,3 +16,20 @@ def guess(x):
 
 #And in this define function computer guess the number.
 def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ' '
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low 
+        feedback = input(f"Is {guess} is too high (H), too low (L), or correct (C)?? ").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+            
+    print(f"Yay! The computer guessed your number, {guess}, correctly!!")
+
+computer_guess(10)
