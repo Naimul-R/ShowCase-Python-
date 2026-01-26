a9fb9e9d497aba90e742e9b9201abed43f55e3fe
Naimul-R <naimul.rabby08@gmail.com>
2026-01-26 11:28:58 +0600

    Working on a new project called hangman

---
+++ Hangman/hangman.py
@@ -0,0 +1,31 @@
+import random
+from words import words 
+import string
+
+def get_valid_word(words):
+    word = random.choice(words) # randomly choose a word from words list 
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) #Letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() #What the user has guessed.
+
+    # Getting user input
+    user_letter = input("Guess a letter: ").upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print("You have already used that letter. Please try again.")
+
+
+user_input = input("Type something: ")
+print(user_input)
