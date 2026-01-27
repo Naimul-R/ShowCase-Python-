2856aed02ecd225d9f4c53084b559e4739309d03
Naimul-R <naimul.rabby08@gmail.com>
2026-01-27 13:12:08 +0600

    this fun game Tic-Tac-Toe project is now done to play.

--- TIC TAC TOE/TTT-game/player.py
+++ TIC TAC TOE/TTT-game/player.py
@@ -28,4 +28,16 @@ class HumanPlayer(Player):
         val = None
         while not valid_square:
             square = input(self.letter + '\'s turn. Input move (0-9): ')
+            # we are going to check that this is a correct value by trying to cast
+            # it to an integer, and if it's not, we say it's invalid
+            # if that spot is not available on the board, we also say it's invalid 
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True # if these are successful, then yay!
+            except ValueError:
+                print("Invalid square. Try again.")
+
+        return val
 
