70eaf3aa756a7d89fd4aa18bc01a786ba41e6065
Naimul-R <naimul.rabby08@gmail.com>
2026-01-28 23:04:41 +0600

    Create QR-code project for generate qr-code image.

--- QR  Code/qr_code2.py
+++ QR  Code/qr_code2.py
@@ -3,6 +3,10 @@ from PIL import Image
 
 qr = qrcode.QRCode(version = 1,
                    error_correction = qrcode.constants.ERROR_CORRECT_H,
-                   box_size= 10, boder= 4)
+                   box_size= 10, border= 10)
 
 qr.add_data("https://www.linkedin.com/in/naimul-rabby-2616743a4/")
+qr.make(fit= True)
+
+img = qr.make_image(fill_color="red" , back_color="white")
+img.save("linkedin_qr.png")
