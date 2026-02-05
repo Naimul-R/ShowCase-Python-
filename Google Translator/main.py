bb6eef7a1936853fb61cdbe483f942ffa585e628
Naimul-R <naimul.rabby08@gmail.com>
2026-02-05 15:24:07 +0600

    Google translate project is now done for translate.

--- Google Translator/main.py
+++ Google Translator/main.py
@@ -3,6 +3,21 @@ from tkinter import ttk
 from googletrans import Translator, LANGUAGES
 
 
+def change(text="type", src="English", dest="Bengali"):
+    text1 = text
+    src1 = src
+    dest1 = dest
+    trans = Translator()
+    trans1 = trans.translate(text, src=src1, dest=dest1)
+    return trans1
+
+def data():
+    s = com_src.get()
+    d = com_dest.get()
+    masg = src_txt.get(1.0, END)
+    textget = change(text = masg, src=s, dest=d)
+    dest_txt.delete(1.0, END)
+    dest_txt.insert(END, textget)
 
 root = Tk()
 root.title("Translator")
@@ -21,13 +36,13 @@ lab_txt.place(x=110, y=100, height=20, width=260)
 src_txt = Text(frame, font=('Time New Roman', 15, 'bold'), wrap=WORD)
 src_txt.place(x=10, y=130, height=150, width=480)
 
-list_text = [1, 2, 3]
+list_text = list(LANGUAGES.values())
 
 com_src = ttk.Combobox(frame, value= list_text)
 com_src.place(x=10, y=300, height=40, width=150)
 com_src.set("English")
 
-button_change = Button(frame, text= "Transtale", relief=RAISED)
+button_change = Button(frame, text= "Transtale", relief=RAISED, command= data)
 button_change.place(x=170, y=300, height=40, width=150)
 
 com_dest = ttk.Combobox(frame, value= list_text)
