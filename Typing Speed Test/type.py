b4be288d3792bde6393d5e55c8a5d51cb43fe72b
Naimul-R <naimul.rabby08@gmail.com>
2026-02-08 14:30:48 +0600

    create one function and also take user input and add time functionality.

--- Typing Speed Test/type.py
+++ Typing Speed Test/type.py
@@ -1,6 +1,18 @@
 from time import *
 import random as r
 
+def mistake(paratest, usertest):
+    error = 0
+    for i in range(len(paratest)):
+        try:
+            if paratest[i] != usertest[i]:
+                error = error + 1
+        except:
+            error = error + 1
+    return error
+
+def time_speed(time_start, time_end, timeinput):
+
 test = [
     "Learning to type faster requires regular practice and focus.",
     "Typing speed improves when accuracy is given priority.",
@@ -17,3 +29,9 @@ test1 = r.choice(test)
 
 print("***** Typing Speed *****")
 print(test1)
+print()
+print()
+
+time_1 = time()
+test_input = input(" Enter: ")
+tine_2 = time()
