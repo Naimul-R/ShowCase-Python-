06c59c353dd2094e4233888343419e652095615b
Naimul-R <naimul.rabby08@gmail.com>
2026-02-12 22:51:15 +0600

    mastermind game project is now ready for play.

--- MasterMind Game/main.py
+++ MasterMind Game/main.py
@@ -17,7 +17,7 @@ def guess_code():
     while True:
         guess = input("Guess: ").upper().split(" ")
 
-        if guess != CODE_LENGTH:
+        if len(guess) != CODE_LENGTH:
             print(f"You must guess {CODE_LENGTH} colors.")
             continue
 
@@ -29,3 +29,45 @@ def guess_code():
             break
 
     return guess
+
+def check_code(guess, real_code):
+    color_count = {}
+    correct_pos = 0
+    incorrect_pos = 0
+
+    for color in real_code:
+        if color not in color_count:
+            color_count[color] = 0
+        color_count[color] += 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color == real_color:
+            correct_pos += 1
+            color_count[guess_color] -= 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color in color_count and color_count[guess_color] > 0:
+            incorrect_pos += 1
+            color_count[guess_color] -= 1
+
+    return correct_pos, incorrect_pos
+
+def game():
+    print(f"Welcome to mastermind, you have {TRIES} to get the code...")
+    print(f"The valid colors are, ", *COLORS)
+    code = generate_code()
+    for attempts in range(1, TRIES + 1):
+        guess = guess_code()
+        correct_pos, incorrect_pos = check_code(guess, code)
+
+        if correct_pos == CODE_LENGTH:
+            print(f"You guessed the code in {attempts} tries!🥳")
+            break
+        
+        print(f"Correct position: {correct_pos} | Incorrect position: {incorrect_pos}")
+
+    else: 
+        print("You ran out of tries, the code was:", *code)
+
+if __name__ == "__main__":
+    game()
