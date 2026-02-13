24ba2d61e6135dade2a47fc48511527b6ec96953
Naimul-R <naimul.rabby08@gmail.com>
2026-02-13 14:58:36 +0600

    Adding bg image perfectly is done.

--- Space-Dodge Game/main.py
+++ Space-Dodge Game/main.py
@@ -6,7 +6,7 @@ WIDTH, HEIGHT = 900, 650
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
-BG = pygame.image.load("Img/")
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
 def draw():
     WIN.blit(BG,(0, 0))
