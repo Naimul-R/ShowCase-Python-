f3547ce72805f23391805c7ec50bf9c4266e3da3
Naimul-R <naimul.rabby08@gmail.com>
2026-02-20 19:14:05 +0600

    Practice or build Cinema Ticket System mini project.

--- a-Topics/Loop/Exercise07.py
+++ a-Topics/Loop/Exercise07.py
@@ -16,12 +16,33 @@ shows = int(input("Enter how many shows are available: "))
 
 total_ticket = 0
 total_revenue = 0
+highest_ticket = 0
+best_show = 0
+
+ticket_price = float(input("Enter ticket price: "))
 
 for i in range(1, shows + 1):
     print(f"\nShows {i}")
 
     seat = int(input("Enter the available seat: "))
+    show_ticket = 0
 
     for j in range(1, seat + 1):
-        status = input("Is this seat sold (yes/no): ")
-        
+        status = input("Is this seat sold (yes/no): ").lower()
+
+        if status == "yes":
+            show_ticket += 1
+            total_ticket += 1
+            total_revenue += ticket_price
+
+    print("Tickets sold in this show: ",show_ticket)
+
+    #Check highest selling show 
+    if show_ticket > highest_ticket:
+        highest_ticket = show_ticket
+        best_show = i
+
+print("\nTotal ticket sold: ",total_ticket)
+print("Total revenue: ",total_revenue)
+print("Best show: ",best_show)
+print("Highest Tickets sold in a show: ",highest_ticket)
