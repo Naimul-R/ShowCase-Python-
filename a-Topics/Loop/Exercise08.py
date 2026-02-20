862cdbfaaeb2a7a1c228c0deafedccf9be2248be
Naimul-R <naimul.rabby08@gmail.com>
2026-02-20 23:05:03 +0600

    solving exercise 8 is now done.

--- a-Topics/Loop/Exercise08.py
+++ a-Topics/Loop/Exercise08.py
@@ -15,11 +15,34 @@
 """
 patients = int(input("Enter the number of patients have: "))
 
-total_bill = 0
+total_revenue = 0
+patients_highest_bill = 0
+highest_bill_amount = 0
 
 for i in range(1, patients + 1):
     print(f"\nPatients {i}: ")
 
-    cost = (input("Enter treatment cost (type 'done' to stop): ")).lower()
-    if cost == 'done':
-        break
+    patients_bill = 0
+
+    while True:
+        cost = input("Enter treatment cost (type 'done' to stop): ").lower()
+
+        if cost == 'done':
+            break
+        
+        cost = float(cost)
+        patients_bill += cost
+
+    print("Total bill for patients", i, "=", patients_bill)
+
+    total_revenue += patients_bill
+
+    # Check highest bill
+    if patients_bill > highest_bill_amount:
+        highest_bill_amount = patients_bill
+        patients_highest_bill = i
+
+print("\nTotal Hospital Revenue = ",total_revenue)
+print("Patients with highest bill = ",patients_highest_bill)
+print("Highest bill amount = ",highest_bill_amount)
+
