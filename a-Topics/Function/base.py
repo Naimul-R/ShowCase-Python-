c952d37a9a47dddd271a363da36525c499044775
Naimul-R <naimul.rabby08@gmail.com>
2026-03-05 22:11:52 +0600

    Argument with function.

--- a-Topics/Function/base.py
+++ a-Topics/Function/base.py
@@ -1,3 +1,22 @@
 # Function - A block of reusable code 
 #            Place () after the funtion name to invoke it.
 
+
+# Argument
+def happy_birthday(name, age):
+    print(f"Happy birthday to you {name}")
+    print(f"You are {age} years old")
+    print("happy birthday to you!")
+    print()
+
+happy_birthday("Anny", 23)
+happy_birthday("Aziz", 25)
+happy_birthday("Himu", 30)
+
+# Exercise - 1
+print("\n======Exercise Code======.")
+def display_invoice(username, amount, due_date):
+    print(f"Hello! {username}.")
+    print(f"Your bill of ${amount:.2f} is due: {due_date}.")
+
+display_invoice("Karim", 85.90, "01/02")
