277ea36d0387e3b77cc698830e36be164fa88889
Naimul-R <naimul.rabby08@gmail.com>
2026-03-14 15:26:15 +0600

    Mini project uisng function for spoerts leaderboard is now done.

--- a-Topics/Function/project-function/01_project.py
+++ a-Topics/Function/project-function/01_project.py
@@ -12,10 +12,28 @@ def add_player():
 
     return players
 
-def showleaderboard(players):
+def show_leaderboard(players):
     print("\n--- Sports Leaderboard ---")
 
     for i, player in enumerate(players, 1):
         print(f"{i}. {player['name']:<10} - Score: {player['score']}")
 
     print("--------------------------")
+
+def get_winner(players):
+    winner = players[0]
+
+    for player in players:
+        if player["score"] > winner["score"]:
+            winner = player
+
+    return winner 
+
+def full_leaderboard():
+    players = add_player()
+    show_leaderboard(players)
+    winner = get_winner(players)
+
+    print(f"\nWinner is {winner['name']} with score {winner['score']}! 🏆")
+
+full_leaderboard()
