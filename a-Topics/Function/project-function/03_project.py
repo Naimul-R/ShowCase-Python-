11894b374bdab573a0fcaa33fb8c15d711e1c516
Naimul-R <naimul.rabby08@gmail.com>
2026-03-18 16:13:13 +0600

    create 4 more function to get the required output.

--- a-Topics/Function/project-function/03_project.py
+++ a-Topics/Function/project-function/03_project.py
@@ -15,3 +15,37 @@ def add_song():
         })
 
     return songs
+
+def show_playlist(songs):
+    print("--- 🎵 Your Playlist ---")
+
+    for i, song in enumerate(songs, 1):
+        print(f"{i}. {song['title']:<20} - Artist {song['artist']:<15} - Duration {song['duration']} mins.")
+        print("------------------------")
+
+def get_total_duration(songs):
+    total = 0
+
+    for song in songs:
+        total += song["duration"]
+    return total
+
+def longest_song(songs):
+    longest = songs[0]
+
+    for song in songs:
+        if song['duration'] > longest['duration']:
+            longest = song
+    return longest
+
+def search_song(songs, keyword):
+    result = []
+
+    for song in songs:
+        if keyword.lower() in song['title'].lower() or \
+        keyword.lower() in song['artist'].lower():
+            result.append(song)
+    return result
+
+
+        
