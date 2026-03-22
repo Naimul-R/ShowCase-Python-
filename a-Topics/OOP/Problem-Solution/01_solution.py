eee00e57ddd274e273c0a6ef1e1b171b38c01c82
Naimul-R <naimul.rabby08@gmail.com>
2026-03-22 23:56:56 +0600

    Problem 1 solution is done using class with attribute and constractor method.

--- a-Topics/OOP/Problem-Solution/01_solution.py
+++ a-Topics/OOP/Problem-Solution/01_solution.py
@@ -1,4 +1,5 @@
 class Car:
+    # __init__ is `Constractor Mehtod`.
     def __init__(self, brand, model):
         # Brand & model are `Attribute`
         self.brand = brand
@@ -6,3 +7,7 @@ class Car:
 
 my_car = Car("BMW", "BMW X7 1")
 print(my_car.brand)
+print(my_car.model)
+
+my_new_car = Car("Audi", "Audi R8")
+print(my_new_car.model)
