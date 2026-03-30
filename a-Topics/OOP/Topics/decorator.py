9ed79490be146efb665e0ec3e54d2022c64fd2ed
Naimul-R <naimul.rabby08@gmail.com>
2026-03-30 23:41:55 +0600

    study how to user decorator in OOP.

--- a-Topics/OOP/Topics/decorator.py
+++ a-Topics/OOP/Topics/decorator.py
@@ -1,1 +1,18 @@
 # Decorator --> A decorator is a function that wraps another function to add extra behavior — without changing the original function's code.
+def greet(fx):
+    def mfx(*arg, **kwargs):
+        print("Good Morning.")
+        fx(*arg, **kwargs)
+        print("Thank you for using this function.")
+    return mfx
+    
+@greet
+def hello():
+    print("Hello World")
+
+@greet
+def add(a, b):
+    print(a+b)
+
+hello()
+add(5, 7)
