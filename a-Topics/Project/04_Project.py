bbb3f213d09a79d777356041b157a1a9616472f8
Naimul-R <naimul.rabby08@gmail.com>
2026-04-12 12:51:49 +0600

    Start building project 4 and create first class Book.

---
+++ a-Topics/Project/04_Project.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        available = True
+        borrower = None
+
+class Library:
+    pass
