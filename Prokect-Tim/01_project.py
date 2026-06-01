829e7ec0ed4578eba4cc46a43be2b29872c5180c
Naimul-R <naimul.rabby08@gmail.com>
2026-06-01 14:57:48 +0600

    Write the left path and right path code for this project game.

--- Prokect-Tim/01_project.py
+++ Prokect-Tim/01_project.py
@@ -29,13 +29,31 @@ if should_play == "y" or should_play == "yes":
 
     # Rigt path ------->
     elif direction == "right":
+        print("You see a bridge ahead...\n")
         choice = input("Okey! You now see a bridge. Do you want to swim under it or cross it? (swim/cross) : ").lower()
         if choice == "swim":
             print("You got eaten by a alligator, you die, the end!!")
-        else: 
-            print("You fonud the gold. And you win!!")
-    else: 
-        print("Sorry! Not a valid reply. You die!!")
+        elif choice == "cross":
+            print("You crossed the bridge safely!\n")
+            next_choice = input("You now see a cave and a village. Where do you go? (cave/village): ").lower()
 
-else: 
-    print("We are NOT playing...")
+            if next_choice == "cave":
+                riddle = input("A troll blocks the cave! He asks: 'What has legs but cannot walk?' (answer): ").lower()
+                if riddle == "table" or riddle == "a table":
+                    print(f"The troll steps aside. You enter the cave and find the gold, {name}! YOU WIN!")
+                else:
+                    print("Wrong answer! The troll throws you off the bridge. Game over!")
+                    
+            elif next_choice == "village":
+                print("You entered the village.")
+                villager = input("A villager offers you a map or a sword. Which do you take? (map/sword): ").lower()
+                if villager == "map":
+                    print(f"The map leads you straight to the hidden treasure, {name}! YOU WIN!")
+                elif villager == "sword":
+                    print("You took the sword but got lost in the woods forever. Game over!")
+                else:
+                    print("You couldn't decide and the village gates closed. Game over!")
+            else:
+                print("You stood there confused and it got dark. Game over!")
+        else:
+            print("Not a valid choice. You slipped into the river. Game over!")
