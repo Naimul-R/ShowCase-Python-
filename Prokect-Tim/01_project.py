6f37c70aa0b655a0e56427128cea72bec994b049
Naimul-R <naimul.rabby08@gmail.com>
2026-06-01 12:29:46 +0600

    start buliding project beginner level to advance level to sharpen my knowlage of python

---
+++ Prokect-Tim/01_project.py
@@ -0,0 +1,9 @@
+name = input("Type your name: ")
+print(f"Hello {name} welcome to my game!")
+
+should_play = input("Do you wnat to play (y/n): ")
+
+if should_play == "y":
+    print("Play continute!")
+else: 
+    
