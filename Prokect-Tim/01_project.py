bd7d4a18dbf4042549342a8311974cc213be006f
Naimul-R <naimul.rabby08@gmail.com>
2026-06-01 12:52:06 +0600

    Add some conditioning using if else statement

--- Prokect-Tim/01_project.py
+++ Prokect-Tim/01_project.py
@@ -1,9 +1,16 @@
 name = input("Type your name: ")
 print(f"Hello {name} welcome to my game!")
 
-should_play = input("Do you wnat to play (y/n): ")
+should_play = input("Do you wnat to play (y/n): ").lower()
 
-if should_play == "y":
+if should_play == "y" or should_play == "yes":
     print("Play continute!")
+
+    direction = input("Do you want to go left or right: ").lower()
+    if direction == "left":
+        print("Okey! We went left.")
+    else: 
+        print("Okey! We went right.")
+
 else: 
-    
+    print("We are NOT playing...")
