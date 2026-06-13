bf39ce745bbd6f8f10b649c26658c0ee6df8fd7c
Naimul-R <naimul.rabby08@gmail.com>
2026-06-13 15:11:34 +0600

    adding frame and colour tto the project

--- Gun-Detection/gun_detection.py
+++ Gun-Detection/gun_detection.py
@@ -15,3 +15,19 @@ while True:
 
     frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    gun = gun_cascade.detectMultiScale(gray,
+                                       1.3, 5,
+                                       minSize=(100, 100))
+    
+    if len(gun) > 0:
+        gun_exist = True
+
+        for (x, y, w, h) in gun:
+            frame = cv2.rectangle(frame,
+                                  (x, y),
+                                  (x + w, y + h),
+                                  (255, 0, 9), 2)
+            
+            roi_gray = gray[y : y + h, x : x + w]
+            roi_color = frame[y : y + h, x : x + w]
