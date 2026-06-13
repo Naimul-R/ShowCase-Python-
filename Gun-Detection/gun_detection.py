67e711b1bc4db1d9cfe76a1db823cbaf86a9096c
Naimul-R <naimul.rabby08@gmail.com>
2026-06-13 14:11:17 +0600

    halfway through this project

--- Gun-Detection/gun_detection.py
+++ Gun-Detection/gun_detection.py
@@ -8,3 +8,10 @@ gun_cascade = cv2.CascadeClassifier('cascade.xml')
 camera = cv2.VideoCapture(0)
 
 firstFrame = None
+gun_exist = None
+
+while True:
+    ret, frame = camera.read()
+
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
