359d2e79027d16db9dc9527b5c06dcf55e827a6e
Naimul-R <naimul.rabby08@gmail.com>
2026-06-14 07:39:39 +0600

    This basic practice mini calculator project is done

--- Project-Tim/02_project.py
+++ Project-Tim/02_project.py
@@ -10,7 +10,19 @@ operand = get_number(1)
 operand2 = get_number(2)
 sign = input("Sign: ")
 
+result = 0
+if sign == "+":
+    result = operand + operand2
+elif sign == "-":
+    result = operand - operand2
+elif sign == "/":
+    if operand2 != 0:
+        result = operand / operand2
+    else:
+        print("Division by zero.")
+elif sign == "*":
+    result = operand * operand2
+else:
+    print("Invalid Sig!!")
 
-
-
-
+print(result)
