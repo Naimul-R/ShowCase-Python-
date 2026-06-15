4e501e7497364a57cfb2de2afcbbbbfc494fc794
Naimul-R <naimul.rabby08@gmail.com>
2026-06-15 11:00:50 +0600

    so far create function where we delete person from list of people

--- Project-Tim/03_project.py
+++ Project-Tim/03_project.py
@@ -10,17 +10,38 @@ def add_person():
     }
     return person
 
+def delete_contact(people):
+    for i, person in enumerate(people):
+        print(i + 1, "-", person["name"], "|", person["age"], "|", person["email"])
+
+    while True:
+        number = input("Enter a number to delete: ")
+        try:
+            number = int(number)
+            if number <= 0 or number > len(people):
+                print("Invalid number, out of number!!")
+            else:
+                break
+        except:
+            print("Invalid Number")
+
+    people.pop(number - 1)
+    print("Person deleted.")
+
 print("Hello!! Welcome to out Contact Managment System.\n")
+
+people = []
+
 while True:
+    print("Contact list size:", len(people))
     command = input("You can 'Add', 'Delete' or 'Search' & 'q' for quit: " ).lower()
-    people = []
 
     if command == "add":
         person = add_person()
         people.append(person)
         print("person added")
     elif command == "delete":
-        pass
+        delete_contact(people)
     elif command == "search":
         pass
     elif command == "q":
