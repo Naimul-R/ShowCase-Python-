e92ec1240cf02382c6874bdd57d426e3aacb327e
Naimul-R <naimul.rabby08@gmail.com>
2026-06-15 09:43:19 +0600

    done wiring add person function and make if else statement

--- Project-Tim/03_project.py
+++ Project-Tim/03_project.py
@@ -1,9 +1,31 @@
-people = []
-
-for i in range(3):
-    print(i + 1, "Input")
+def add_person():
     name = input("Name: ")
     age = input("Age: ")
     email = input("Email: ")
 
-    
+    person = {
+        "name": name,
+        "age": age,
+        "email": email
+    }
+    return person
+
+print("Hello!! Welcome to out Contact Managment System.\n")
+while True:
+    command = input("You can 'Add', 'Delete' or 'Search' & 'q' for quit: " ).lower()
+    people = []
+
+    if command == "add":
+        person = add_person()
+        people.append(person)
+        print("person added")
+    elif command == "delete":
+        pass
+    elif command == "search":
+        pass
+    elif command == "q":
+        break
+    else:
+        print("Invalid Command!!")
+
+
