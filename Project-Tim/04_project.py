5b71591587798b0bcc725339be900b61c5aac7ce
Naimul-R <naimul.rabby08@gmail.com>
2026-06-16 21:01:47 +0600

    Now working on another practice project

---
+++ Project-Tim/04_project.py
@@ -0,0 +1,10 @@
+def print_board(board):
+    for i, row in enumerate(board):
+        for j, col in enumerate(row):
+            pass
+
+board = [
+    [" ", " ", " "]
+    [" ", " ", " "]
+    [" ", " ", " "]
+]
