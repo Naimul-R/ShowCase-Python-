8334c2ff51146b10f3035191d90f0cdc6e80835d
Naimul-R <naimul.rabby08@gmail.com>
2026-06-16 21:34:39 +0600

    create to funciotn one is for board and another is for movement

--- Project-Tim/04_project.py
+++ Project-Tim/04_project.py
@@ -1,10 +1,32 @@
 def print_board(board):
     for i, row in enumerate(board):
-        for j, col in enumerate(row):
-            pass
+        row_str = " "
+        for j, value in enumerate(row):
+            row_str += value
+            if j != len(row) -1:
+                row_str += " | "
+
+        print(row_str)
+        if i != len(board) - 1:
+            print("----------")
+
+def get_move(turn, board):
+    while True:
+        row = int(input("Row: "))
+        col = int(input("Col: "))
+
+        if row < 1 or row > len(board):
+            print("Invalid row, try again!!")
+            continue
+        if col < 1 or col > len(board[row]):
+            print("Invalid col, try again!!")
+            continue
+
 
 board = [
-    [" ", " ", " "]
-    [" ", " ", " "]
+    [" ", " ", " "],
+    [" ", " ", " "],
     [" ", " ", " "]
 ]
+
+print_board(board)
