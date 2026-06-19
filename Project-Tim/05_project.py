674da315d3e241cc0dc345a26c06e501f961100e
Naimul-R <naimul.rabby08@gmail.com>
2026-06-19 15:39:07 +0600

    create two function and add two more question to this project 5

--- Project-Tim/05_project.py
+++ Project-Tim/05_project.py
@@ -6,3 +6,21 @@ def load_question():
         questions = json.load(f)["questions"]
         
     return questions
+
+def get_random_questions(questions, num_questions):
+    if num_questions > len(questions):
+        num_questions = len(questions)
+        
+    random_questions = random.sample(questions, num_questions)
+    return random_questions
+
+def ask_question(question):
+    print(question["question"])
+    for i, option in enumerate(question["options"]):
+        print(str(i + 1) + ".", option)
+    
+    numbers = int(input("Select the correct number: "))
+
+question = load_question()
+random_questions = get_random_questions(question, 2)
+print(random_questions)
