fa393b816068dedcff14704a95bf8a66a21418ac
Naimul-R <naimul.rabby08@gmail.com>
2026-07-02 23:40:56 +0600

    create text1 label for Encryption

--- Encryption & Decryption/main.py
+++ Encryption & Decryption/main.py
@@ -5,3 +5,17 @@ import os
 
 def main_screen():
     screen=Tk()
+    screen.geometry("375x398")
+    
+    #Icon
+    image_icon = PhotoImage(file='keys.png')
+    screen.iconphoto(False,image_icon)
+    screen.title("PctApp")
+    
+    Label(text="Enter the text for Encryption & Decryption", fg="black", font=("calbri", 13)).place(x=10,y=10)
+    text1 = Text(font="Robote 16",bg="white",relief=GROOVE,wrap=WORD,bd=0)
+    text1.place(x=10,y=50,width=355,height=100)
+    
+    screen.mainloop()
+    
+main_screen()
