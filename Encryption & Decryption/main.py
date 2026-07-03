2125e7bda814e22e3a423878f6a5d270baf82258
Naimul-R <naimul.rabby08@gmail.com>
2026-07-03 21:32:34 +0600

    create second Label for secrect key

--- Encryption & Decryption/main.py
+++ Encryption & Decryption/main.py
@@ -16,6 +16,8 @@ def main_screen():
     text1 = Text(font="Robote 16",bg="white",relief=GROOVE,wrap=WORD,bd=0)
     text1.place(x=10,y=50,width=355,height=100)
     
+    Label(text="Enter secrect key for encryption and decryption",fg="black",font=("calbri",13)).place(x=10,y=170)
+    
     screen.mainloop()
     
 main_screen()
