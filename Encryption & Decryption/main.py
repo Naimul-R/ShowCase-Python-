eceb42ec7cca642dd86f0cbbc17597fc9856074d
Naimul-R <naimul.rabby08@gmail.com>
2026-07-03 22:04:55 +0600

    working on function encryption to entrypt data

--- Encryption & Decryption/main.py
+++ Encryption & Decryption/main.py
@@ -3,7 +3,32 @@ from tkinter import messagebox
 import base64
 import os 
 
+def decrypt():
+    print()
+    
+def encrypt():
+    password=code.get()
+    
+    if password == "1234":
+        screen1=Toplevel(screen)
+        screen1.title("encryption")
+        screen1.geometry("400x200")
+        screen1.configure(bg="#ed3833")
+        
+        message=text1.get(1.0,END)
+        encode_message=message.encode("ascii")
+        base64_bytes=base64.b64decode(encode_message)
+        encrypt=base64_bytes.decode("ascii")
+        
+        Label(screen1,text="ENCRYPT",font="arial",fg="white",bg="#ed3833").place(x=10,y=0)
+
+
 def main_screen():
+    
+    global screen
+    global code
+    global text1
+    
     screen=Tk()
     screen.geometry("375x398")
     
@@ -11,6 +36,10 @@ def main_screen():
     image_icon = PhotoImage(file='keys.png')
     screen.iconphoto(False,image_icon)
     screen.title("PctApp")
+    
+    def reset():
+        code.set("")
+        text1.delete(1.0,END)
     
     Label(text="Enter the text for Encryption & Decryption", fg="black", font=("calbri", 13)).place(x=10,y=10)
     text1 = Text(font="Robote 16",bg="white",relief=GROOVE,wrap=WORD,bd=0)
@@ -21,8 +50,9 @@ def main_screen():
     code=StringVar()
     Entry(textvariable=code,width=19,bd=0,font=("arial",25),show="*").place(x=10,y=200)
     
-    Button(text='ENCRYPT',height=2,width=23,bg='#ed3833',fg="white",bd=0).place(x=10,y=250)
-    Button(text='DECRYPT',height=2,width=23,bg="#00bd56",fg="white",bd=0).place(x=200,y=250)
+    Button(text='ENCRYPT',height=2,width=23,bg='#ed3833',fg="white",bd=0,command=encrypt).place(x=10,y=250)
+    Button(text='DECRYPT',height=2,width=23,bg="#00bd56",fg="white",bd=0,command=decrypt).place(x=200,y=250)
+    Button(text="RESET",height=2,width=50,bg="#1089ff",fg="white",bd=0,command=reset).place(x=10,y=300)
     
     screen.mainloop()
     
