b212d964fe4bb4946e33976e41d5e020b8287ebc
Naimul-R <naimul.rabby08@gmail.com>
2026-07-05 22:44:15 +0600

    create funciton for login and also two entry for password & username.

--- login-form/setup.py
+++ login-form/setup.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import messagebox
 
+def login():
+    pass
+
 root=Tk()
 root.title("Login")
 root.geometry("300x200")
@@ -9,6 +12,15 @@ global entry1
 global entry2
 
 Label(root,text="Username").place(x=20,y=20)
+Label(root,text="Password").place(x=20,y=70)
+
+entry1=Entry(root,bd=5)
+entry1.place(x=140,y=20)
+
+entry2=Entry(root,bd=5)
+entry2.place(x=140,y=70)
+
+Button(root,text="Login",command=login,height=3,width=13,bd=5).place(x=100,y=120)
 
 
 root.mainloop()
