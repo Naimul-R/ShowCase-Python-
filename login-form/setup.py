768ad9fb08206ac8ceda2e7ec0e27916137f44cb
Naimul-R <naimul.rabby08@gmail.com>
2026-07-05 22:33:44 +0600

    Take two global entry and create one Lavel for username

--- login-form/setup.py
+++ login-form/setup.py
@@ -1,3 +1,14 @@
 from tkinter import *
 from tkinter import messagebox
 
+root=Tk()
+root.title("Login")
+root.geometry("300x200")
+
+global entry1
+global entry2
+
+Label(root,text="Username").place(x=20,y=20)
+
+
+root.mainloop()
