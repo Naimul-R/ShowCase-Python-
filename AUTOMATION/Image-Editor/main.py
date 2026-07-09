2cf3edf52a79c075fd15e352e4763c5e8cce05b8
Naimul-R <naimul.rabby08@gmail.com>
2026-07-09 23:25:57 +0600

    Image editor project is now ready to execute

--- AUTOMATION/Image-Editor/main.py
+++ AUTOMATION/Image-Editor/main.py
@@ -2,14 +2,20 @@ from PIL import Image, ImageEnhance, ImageFilter
 import os
 
 path = './imgs'
-path_out = '/editedImgs'
+path_out = './editedImgs'
+
+os.makedirs(path_out, exist_ok=True)
 
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
     
     #sharpen
-    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-99)
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90, expand=True)
+    
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
     
     clean_name = os.path.splitext(filename)[0]
 
-    edit.save(f'.{path_out}/{clean_name}_edited.jpg')
+    edit.save(f'{path_out}/{clean_name}_edited.jpg')
