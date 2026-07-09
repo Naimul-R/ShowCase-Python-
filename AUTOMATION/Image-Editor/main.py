54f5adc1812b4092c58c388abe7c5da097ac984c
Naimul-R <naimul.rabby08@gmail.com>
2026-07-09 23:07:46 +0600

    define path variable and add sharpen function

--- AUTOMATION/Image-Editor/main.py
+++ AUTOMATION/Image-Editor/main.py
@@ -1,2 +1,15 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
+
+path = './imgs'
+path_out = '/editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    
+    #sharpen
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-99)
+    
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'.{path_out}/{clean_name}_edited.jpg')
