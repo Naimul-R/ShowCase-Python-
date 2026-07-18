2c32ef5b67a632b1a5b24e44687119c0b086b04a
Naimul-R <naimul.rabby08@gmail.com>
2026-07-19 00:34:47 +0600

    create if else statement to save, load and list the data

--- AUTOMATION/Mini-Clipboard/main.py
+++ AUTOMATION/Mini-Clipboard/main.py
@@ -1,3 +1,17 @@
 import sys
 import clipboard 
 import json
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    
+    if command == "save":
+        print("Save")
+    elif command == "load":
+        print("Load")
+    elif command == "list":
+        print("List")
+    else:
+        print("Unknown Command!")
+else:
+    print("Please pass excatly one command!!")
