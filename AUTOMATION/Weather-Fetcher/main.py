93b12f32d33824681f09bd1b6f8ce51c09f9fb69
Naimul-R <naimul.rabby08@gmail.com>
2026-07-20 20:49:37 +0600

    import request module, add api, base url and response request

--- AUTOMATION/Weather-Fetcher/main.py
+++ AUTOMATION/Weather-Fetcher/main.py
@@ -1,1 +1,8 @@
-API_KEY = ""
+import requests
+
+API_KEY = "2bbeda463cc69a3f61f692d040f85837"
+BASE_URL =  "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
