2185edc6d6926a35f1c1b90df75c078541a9654d
Naimul-R <naimul.rabby08@gmail.com>
2026-07-21 16:30:06 +0600

    halfway through done to build this project

--- AUTOMATION/Email-sender/main.py
+++ AUTOMATION/Email-sender/main.py
@@ -8,3 +8,10 @@ sender_email = "coder.me08@gmail.com"
 receiver_email = "coder.me08@gmail.com"
 password = input("Enter a password: ")
 
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)
+
+context = ssl.create_default_context()
