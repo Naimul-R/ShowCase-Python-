23c04a170124c0ec774c935bc32acfc83dbe9d87
Naimul-R <naimul.rabby08@gmail.com>
2026-07-21 16:40:28 +0600

    Now email sender automation project is executable

--- AUTOMATION/Email-sender/main.py
+++ AUTOMATION/Email-sender/main.py
@@ -12,6 +12,24 @@ message = EmailMessage()
 message["From"] = sender_email
 message["To"] = receiver_email
 message["Subject"] = subject
-message.set_content(body)
+
+html = f"""
+<html>
+    <body>
+        <h1>{subject}</h1>
+        <p>{body}</p>
+    </body>
+</html>
+"""
+
+message.add_alternative(html, subtype="html")
 
 context = ssl.create_default_context()
+
+print("Sending email....")
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+
+print("Success!")
