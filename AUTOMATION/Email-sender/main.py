a83a687e386593f7502590db04e4db81c849905e
Naimul-R <naimul.rabby08@gmail.com>
2026-07-21 16:25:25 +0600

    create variables for subject, body, sender and receiver email with input password

--- AUTOMATION/Email-sender/main.py
+++ AUTOMATION/Email-sender/main.py
@@ -1,3 +1,10 @@
 import smtplib
 import ssl
 from email.message import EmailMessage
+
+subject = "Email From Python"
+body = "This is a test email from python"
+sender_email = "coder.me08@gmail.com"
+receiver_email = "coder.me08@gmail.com"
+password = input("Enter a password: ")
+
