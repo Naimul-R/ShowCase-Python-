cb43351f0dfbbe77e8688ff99c88c7e99c9b3fda
Naimul-R <naimul.rabby08@gmail.com>
2026-07-23 23:56:23 +0600

    create function for finance manager to automate

--- AUTOMATION/Finance-Manager/financem.py
+++ AUTOMATION/Finance-Manager/financem.py
@@ -1,1 +1,21 @@
 import csv 
+
+month = "July"
+
+def finance_manager(file):
+    sum = 0 
+    transiction = []
+    
+    with open(file, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        header = next(csv_reader)
+        for row in csv_reader:
+            # Get name, amount and currency 
+            name = row[4]
+            amount = float(row[7])
+            date = row[1]
+            
+            transiction = (date, name, amount)
+            sum += amount
+            
+            
